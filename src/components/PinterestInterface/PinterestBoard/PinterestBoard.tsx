76f475fb9381e83105c5e9aec997f333106fcdcb
die-h <diegohermida94@gmail.com>
2022-12-07 09:13:56 -0300

    Completed Pinterest interface

--- src/components/PinterestInterface/PinterestBoard/PinterestBoard.tsx
+++ src/components/PinterestInterface/PinterestBoard/PinterestBoard.tsx
@@ -1,57 +1,42 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 import style from "./PinterestBoard.module.scss";
 import { PinterestCard } from "components/PinterestInterface";
 
 const PinterestBoard = () => {
+  const cardCreator = useRef(null);
+  const [isCreatorvisible, setIsCreatorVisible] = useState(false);
+  const [imgAmmount, setImgAmmount] = useState<number[]>([235, 236, 237]);
+
+  const handleCreator = () => {
+    if (isCreatorvisible) {
+      let aux = [...imgAmmount];
+      for (let i = 0; i < 20; i++) {
+        aux.push(aux[aux.length - 1] + 1);
+      }
+      setImgAmmount(aux);
+    } else {
+      return;
+    }
+  };
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      const entry = entries[0];
+      setIsCreatorVisible(entry.isIntersecting);
+    });
+    observer.observe(cardCreator.current!);
+  }, []);
+
+  useEffect(() => {
+    handleCreator();
+  }, [isCreatorvisible]);
+
+  const testArray: number[] = [2, 5, 6, 7];
   return (
     <main className={style.main}>
-      <PinterestCard />
-      <PinterestCard />
-      <PinterestCard />
-      <PinterestCard />
-      <PinterestCard />
-      <PinterestCard />
-      <PinterestCard />
-      <PinterestCard />
-      <PinterestCard />
-      <PinterestCard />
-      <PinterestCard />
-      <PinterestCard />
-      <PinterestCard />
-      <PinterestCard />
-      <PinterestCard />
-      <PinterestCard />
-      <PinterestCard />
-      <PinterestCard />
-      <PinterestCard />
-      <PinterestCard />
-      <PinterestCard />
-      <PinterestCard />
-      <PinterestCard />
-      <PinterestCard />
-      <PinterestCard />
-      <PinterestCard />
-      <PinterestCard />
-      <PinterestCard />
-      <PinterestCard />
-      <PinterestCard />
-      <PinterestCard />
-      <PinterestCard />
-      <PinterestCard />
-      <PinterestCard />
-      <PinterestCard />
-      <PinterestCard />
-      <PinterestCard />
-      <PinterestCard />
-      <PinterestCard />
-      <PinterestCard />
-      <PinterestCard />
-      <PinterestCard />
-      <PinterestCard />
-      <PinterestCard />
-      <PinterestCard />
-      <PinterestCard />
-      <PinterestCard />
+      {imgAmmount.map((item, index) => {
+        return <PinterestCard key={index} size={item} />;
+      })}
+      <div ref={cardCreator}></div>
     </main>
   );
 };
--- src/components/PinterestInterface/PinterestCard/PinterestCard.tsx
+++ src/components/PinterestInterface/PinterestCard/PinterestCard.tsx
@@ -1,10 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import style from "./PinterestCard.module.scss";
+import { getRandomBetweenTwoNumbers } from "utils/getRandomBetweenTwoNumbers";
+import { RiUpload2Line } from "react-icons/ri";
+import { HiDotsHorizontal } from "react-icons/hi";
 
-const PinterestCard = () => {
+interface ImgSize {
+  size: number;
+}
+
+const PinterestCard = ({ size }: ImgSize) => {
+  const [rowSize, setRowSize] = useState(3);
+  const [isOver, setIsOver] = useState(false);
+
+  const handleMouseOver = (event: React.MouseEvent<HTMLElement>) => {
+    setIsOver(true);
+  };
+  const handleMouseOut = (event: React.MouseEvent<HTMLElement>) => {
+    setIsOver(false);
+  };
+
+  useEffect(() => {
+    const randomRow = getRandomBetweenTwoNumbers(6, 12);
+    setRowSize(randomRow);
+  }, []);
+
   return (
-    <article className={style.cardContainer}>
-      <img src="http://picsum.photos/300" alt="" />
+    <article
+      style={{ gridRow: `span ${rowSize}` }}
+      className={style.cardContainer}
+      onMouseOver={handleMouseOver}
+      onMouseOut={handleMouseOut}
+    >
+      <img src={`http://picsum.photos/${size}`} alt="" />
+      <button type="button" className={isOver ? style.saveButton : "inactive"}>
+        Guardar
+      </button>
+      <div className={isOver ? style.butContainer : "inactive"}>
+        <button type="button">
+          <HiDotsHorizontal />
+        </button>
+        <button type="button">
+          <RiUpload2Line />
+        </button>
+      </div>
     </article>
   );
 };
